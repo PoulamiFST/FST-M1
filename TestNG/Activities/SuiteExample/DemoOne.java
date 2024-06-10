89d2a2758bbeecc4dae42d253b0d2e4555272faa
PoulamiFST <pouladas@in.ibm.com>
2024-06-10 11:46:44 +0530

    Add files via upload

---
+++ TestNG/Activities/SuiteExample/DemoOne.java
@@ -0,0 +1,14 @@
+package SuiteExample;
+
+import org.testng.annotations.Test;
+
+public class DemoOne {
+    @Test
+    public void firstTestCase(){
+        System.out.println("I am in first test case from Demo one");
+    }
+    @Test
+    public void secondTestCase(){
+        System.out.println("I am in second test case from Demo one");
+    }
+}
---
+++ TestNG/Activities/SuiteExample/DemoTwo.java
@@ -0,0 +1,10 @@
+package SuiteExample;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+    @Test
+    public void TestCase(){
+        System.out.println("I am in the test case from Demo Two");
+    }
+}
